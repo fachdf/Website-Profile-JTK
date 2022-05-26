c494f2f7909ea37dcad58c8f00817bc56cfb7b10
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2022-05-27 00:08:09 +0700

    :sparkle: Add Tracer Study page

---
+++ frontend/src/components/TracerStudyD3/TCD3Sebaran.vue
@@ -0,0 +1,21 @@
+<template>
+
+ <div align=center  class="title">
+      <p align=center class="display-2 mt-10 pl-10 btext font-weight-bold" >Tingkat Sebaran Pekerjaan</p>
+    <v-sheet
+        height="10"
+        width="100%"
+        color="white"
+      > 
+      </v-sheet>
+    <div align=right class="kelulusan">
+       <p align=left class="btext mt-20 pt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam accumsan mauris non ultrices. Vestibulum malesuada consectetur ornare. Phasellus elementum, metus non commodo blandit, ligula neque porta neque, a luctus mi sapien at libero. Cras sit amet eros in tellus tempus porta. Sed bibendum mauris et hendrerit varius. Praesent maximus metus ut nulla tempor, non tempus sapien gravida. Nam vel ultrices lectus, ut iaculis mi. Donec non odio lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur cursus nisi lectus, in porta velit molestie sed. Quisque ornare nunc vitae turpis condimentum, eget consequat justo tincidunt. Aenean tincidunt iaculis mi, vitae dignissim elit sodales quis.</p>
+    </div>  
+</div>
+</template>
+
+<style scoped>
+.title{
+    width:60%
+}
+</style>
