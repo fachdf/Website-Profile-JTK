ea7741b9d155643ed16fce980c66d845fe15e747
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2022-05-27 14:17:10 +0700

    :lipstick: Update icon and title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    iconPaths: {
+        favicon32: 'favicon.ico',
+        favicon16: 'favicon.ico',
+        appleTouchIcon: 'favicon.ico',
+        maskIcon: 'favicon.ico',
+        msTileImage: 'favicon.ico'
+    }
+}
 }
