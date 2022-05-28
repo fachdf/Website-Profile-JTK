2e1d22b339678a0789b33ad79ae209c42a01a19d
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2022-05-29 02:06:04 +0700

    :lipstick: Update kurikulum page

---
+++ frontend/src/components/Kurikulum/KurikulumD3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div align=center class="title">
+    <p class="display-2 btext font-weight-bold" >D3 Teknik Informatika</p>
+    <v-sheet
+      margin-top="10"
+      height="5"
+      width="600"
+      color="#FF6600"> 
+    </v-sheet>
+    <v-sheet
+      height="30"
+      width="100%"
+      color="white"> 
+    </v-sheet>
+    <v-img
+      max-height="500"
+      max-width="600"
+      src="https://res.cloudinary.com/kelompok-b7/image/upload/v1653569101/sarana_papan_jurusan_560x300_7df440b18e.jpg">
+    </v-img>
+    <p align=left class="btext mt-5" style="width:70%">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultrices bibendum nunc. Fusce dictum dui enim, sit amet ultricies odio dictum id. Curabitur dapibus dui velit, quis iaculis urna mollis eu. In dictum turpis eu commodo consectetur. Curabitur feugiat lectus ac urna placerat, a dignissim sem pretium. Praesent ac luctus ipsum. Pellentesque consectetur tellus eu molestie tincidunt. Phasellus pellentesque ante vitae risus vulputate hendrerit. Curabitur ullamcorper vel magna convallis dignissim.</p>
+    <v-sheet
+      height="30"
+      width="100%"
+      color="white"> 
+    </v-sheet>
+  </div>
+</template>
+
+<style scoped>
+  .title{
+      width:100%;
+  }
+</style>
--- frontend/src/views/Kurikulum.vue
+++ frontend/src/views/Kurikulum.vue
@@ -1,67 +1,56 @@
 <template>
-
-    <div>
-        <v-carousel
-    cycle
-    height="400"
-    hide-delimiter-background
-    show-arrows-on-hover
-  >
-    <v-carousel-item
-      v-for="(slide, i) in slides"
-      :key="i"
-      :src="slide.img"
-    >
+  <div>
+    <v-carousel
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover>
+      <v-carousel-item
+        v-for="(slide, i) in slides"
+          :key="i"
+          :src="slide.img">
         <v-row
           class="fill-height"
           align="center"
-          justify="center"
-        >
-        
-        <div class=" black white--text text-h3" align="center"
           justify="center">
-            {{ slide.text }} <br>
-            <v-sheet
-        color="#FF6600"
-        height="5"
-        width="455"
-      ></v-sheet>
-        </div>
-        
+          <div class=" black white--text text-h3" align="center" justify="center">
+              {{ slide.text }} <br>
+              <v-sheet
+                color="#FF6600"
+                height="5"
+                width="455">
+              </v-sheet>
+          </div>  
         </v-row>
-        
-    </v-carousel-item>
-  </v-carousel>
-  <v-breadcrumbs
+      </v-carousel-item>
+    </v-carousel>
+    <v-breadcrumbs
       :items="items"
       divider=">"
       small
-      class="ml-9"
-    ></v-breadcrumbs>
-        
-        <KurikulumD4/>
-        <KurikulumD3/>
-    </div>
-    
-
+      class="ml-9">
+    </v-breadcrumbs>
+    <KurikulumD3/>
+    <KurikulumD4/>
+  </div>
 </template>
-<style>
-.b {
-   color : #111F4D 
-}
 
+<style>
+  .b {
+    color : #111F4D 
+  }
 </style>
+
 <script>
-import axios from "axios";
-import KurikulumD4 from "../components/KurikulumD4.vue";
-import KurikulumD3 from "../components/KurikulumD3.vue";
+  import axios from "axios";
+  import KurikulumD3 from "../components/Kurikulum/KurikulumD3.vue";
+  import KurikulumD4 from "../components/Kurikulum/KurikulumD4.vue";
 
   export default {
     components: {
-    KurikulumD4,
-    KurikulumD3
-   },
-
+      KurikulumD3,
+      KurikulumD4
+    },
     data () {
       return {
         carousel: [],
@@ -73,42 +62,30 @@ import KurikulumD3 from "../components/KurikulumD3.vue";
           'red lighten-1',
           'deep-purple accent-4',
         ],
-        slides: [{
+        slides: [
+          {
           img : "https://res.cloudinary.com/kelompok-b7/image/upload/v1653566467/Group_208_991116458a.png",
           text : "Kurikulum"
-        }
+          }
         ],
         items: [
-        {
+          {
           text: 'Home',
           disabled: false,
           href: '/',
-        },
-        {
-          text: 'Kurikulum',
-          disabled: true,
-          href: '/kurikulum',
-        }
-      ],
-      riwayatJTK_text : ""
-        
+          },
+          {
+            text: 'Kurikulum',
+            disabled: true,
+            href: '/kurikulum',
+          }
+        ]        
       }
-
     },
     beforeMount() {
-    this.fetchData();
-   },
-   methods: {
-    async fetchData() {
-      const riwayat = await axios.get(this.CMS_API + "/riwayat-jtk");
-      var i;
-      this.riwayatJTK_text = riwayat.data.riwayatJTK
-      console.log(riwayat.data)
+      this.fetchData();
     },
-  },
+    methods: {
+    },
   }
 </script>
-
-<style >
-
-</style>
