bb3f8a900a1ab7310ea30007148655c138502017
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2022-05-29 02:06:58 +0700

    :lipstick: Updaye daftar prestasi page

--- frontend/src/components/DaftarPrestasi/DPPenghargaan.vue
+++ frontend/src/components/DaftarPrestasi/DPPenghargaan.vue
@@ -1,63 +1,61 @@
 <template>
-    <div align="center" class="title">
-      <p class="display-2 mt-2 btext font-weight-bold" >Penghargaan</p>
-      <v-sheet
-        color="#FF6600"
-        height="5"
-        width="155"
-      ></v-sheet>
-
-      <v-row no-gutters class="my-3" >
-        <v-col
-          v-for="(carousel, i) in this.carousel"
-            :key="i"
+  <div align="center" class="title">
+    <v-sheet
+      height="30"
+      width="100%"
+      color="white"> 
+    </v-sheet>
+    <p class="display-2 btext font-weight-bold">Penghargaan</p>
+    <v-sheet
+      color="#FF6600"
+      height="5"
+      width="350">
+    </v-sheet>
+    <v-row no-gutters class="my-3" >
+      <v-col
+        v-for="(carousel, i) in this.carousel"
+          :key="i"
           cols="5"
           sm="2"
-          class="mx-auto"
-        >
-            <v-card
-              :loading="loading"
-              class="mx-auto my-12"
-              max-width="374"
-             
-            >
-
-    <v-img
-      height="250"
-       :src='carousel.gambar_gallery[0].url'
-    ></v-img>
-
-    <v-card-title>{{carousel.keterangan_gallery}}</v-card-title>
-
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0 mt-2"
-
-      >
-        <v-icon>mdi-calendar-blank</v-icon>
-        <div class="grey--text ms-4">
-          {{carousel.tanggal_gallery}}
-        </div>
-      </v-row>
-    </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-    </div>
+          class="mx-auto">
+        <v-card
+          :loading="loading"
+          class="mx-auto my-12"
+          max-width="374">
+          <v-img
+            height="250"
+            :src='carousel.gambar_gallery[0].url'>
+          </v-img>
+          <v-card-title>
+            {{carousel.keterangan_gallery}}
+          </v-card-title>
+          <v-card-text>
+            <v-row
+              align="center"
+              class="mx-0 mt-2">
+              <v-icon>mdi-calendar-blank</v-icon>
+              <div class="grey--text ms-4">
+                {{carousel.tanggal_gallery}}
+              </div>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
-<style>
-.btext {
-   color : #111F4D 
-}
 
-.title {
-  border-bottom-color: #FF6600;
-}
+<style>
+  .btext {
+    color : #111F4D 
+  }
+  .title {
+    border-bottom-color: #FF6600;
+  }
 </style>
+
 <script>
 import axios from "axios";
-
   export default {
     data () {
       return {
@@ -65,17 +63,16 @@ import axios from "axios";
       }
     },
     beforeMount() {
-    this.fetchData();
-   },
-   methods: {
-    async fetchData() {
-      const carousel = await axios.get("http://localhost:1337/galleries");
-      var i;
-      for (let i = 0; i < 5; i++) {
-        this.carousel.push(carousel.data[i])
-      }
+      this.fetchData();
     },
-    
-  },
+    methods: {
+      async fetchData() {
+        const carousel = await axios.get("http://localhost:1337/galleries");
+        var i;
+        for (let i = 0; i < 5; i++) {
+          this.carousel.push(carousel.data[i])
+        }
+      },
+    },
   }
 </script>
