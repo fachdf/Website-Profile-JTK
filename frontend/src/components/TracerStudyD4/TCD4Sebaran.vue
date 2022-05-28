6241cb0c2e2a76f5ac5c1fe7d557655ff010dd23
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2022-05-29 02:06:43 +0700

    :lipstick: Update tracer study page

--- frontend/src/components/TracerStudyD4/TCD4Sebaran.vue
+++ frontend/src/components/TracerStudyD4/TCD4Sebaran.vue
@@ -1,21 +1,29 @@
 <template>
-
- <div align=center  class="title">
-      <p align=center class="display-2 mt-10 pl-10 btext font-weight-bold" >Tingkat Sebaran Pekerjaan</p>
-    <v-sheet
-        height="10"
+  <div align=center  class="title">
+    <p class="display-2 btext">Tingkat Sebaran Pekerjaan</p>
+    <div>
+      <v-img
+        max-height="500"
+        max-width="600"
+        src="https://res.cloudinary.com/kelompok-b7/image/upload/v1653569101/sarana_papan_jurusan_560x300_7df440b18e.jpg">
+      </v-img>
+      <v-sheet
+        height="20"
         width="100%"
-        color="white"
-      > 
+        color="white">
+      </v-sheet> 
+      <p align=left class="btext">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam accumsan mauris non ultrices. Vestibulum malesuada consectetur ornare. Phasellus elementum, metus non commodo blandit, ligula neque porta neque, a luctus mi sapien at libero. Cras sit amet eros in tellus tempus porta. Sed bibendum mauris et hendrerit varius. Praesent maximus metus ut nulla tempor, non tempus sapien gravida. Nam vel ultrices lectus, ut iaculis mi. Donec non odio lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur cursus nisi lectus, in porta velit molestie sed. Quisque ornare nunc vitae turpis condimentum, eget consequat justo tincidunt. Aenean tincidunt iaculis mi, vitae dignissim elit sodales quis.</p>
+      <v-sheet
+        height="20"
+        width="100%"
+        color="white">
       </v-sheet>
-    <div align=right class="kelulusan">
-       <p align=left class="btext mt-20 pt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam accumsan mauris non ultrices. Vestibulum malesuada consectetur ornare. Phasellus elementum, metus non commodo blandit, ligula neque porta neque, a luctus mi sapien at libero. Cras sit amet eros in tellus tempus porta. Sed bibendum mauris et hendrerit varius. Praesent maximus metus ut nulla tempor, non tempus sapien gravida. Nam vel ultrices lectus, ut iaculis mi. Donec non odio lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur cursus nisi lectus, in porta velit molestie sed. Quisque ornare nunc vitae turpis condimentum, eget consequat justo tincidunt. Aenean tincidunt iaculis mi, vitae dignissim elit sodales quis.</p>
     </div>  
-</div>
+  </div>
 </template>
 
 <style scoped>
-.title{
-    width:60%
-}
+  .title{
+      width:70%
+  }
 </style>
--- frontend/src/components/TracerStudyList.vue
+++ frontend/src/components/TracerStudyList.vue
@@ -1,87 +1,73 @@
 <template>
-    <div align="center" class="title">
-      <v-row no-gutters class="my-3" >
-        <v-col
-          v-for="(carousel, i) in this.fasilitas"
-            :key="i"
-          cols="2"
+  <div align="center" class="title">
+    <v-row no-gutters class="my-3">
+      <v-col
+        v-for="(carousel, i) in this.fasilitas"
+          :key="i"
           sm="3"
-          class="mx-auto"
-        >
-            <v-card
-              :loading="loading"
-              class="mx-auto my-12"
-              max-width="700"
-              :href="carousel.link"
-            >
-
-    <v-img
-      width="500"
-       :src='carousel.gambar_fasilitas'
-       :href='carousel.link'
-    ></v-img>
-
-    <v-card-title class="justify-center"><b >{{carousel.nama_fasilitas}}</b></v-card-title>
-
-            </v-card>
-          </v-col>
-        </v-row>
-    
-    
-    </div>
+          class="mx-auto">
+          <v-card
+            :loading="loading"
+            class="mx-auto my-12"
+            max-width="700"
+            :href="carousel.link">
+            <v-img
+              width="500"
+              :src='carousel.gambar_fasilitas'
+              :href='carousel.link'>
+            </v-img>
+            <v-card-title class="justify-center">
+              <b>{{carousel.nama_fasilitas}}</b>
+            </v-card-title>
+          </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
-<style>
-.btext {
-   color : #111F4D 
-}
 
-.title {
-  border-bottom-color: #FF6600;
-}
+<style>
+  .btext {
+    color : #111F4D 
+  }
+  .title {
+    border-bottom-color: #FF6600;
+  }
 </style>
+
 <script>
-import axios from "axios";
+  // import axios from "axios";
 
   export default {
     data () {
       return {
-        fasilitas: [{
-          gambar_fasilitas : "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572229/lab_e3c81fd48c.png",
-          nama_fasilitas : "D3 Teknik Informatika",
-          link : "/tracerStudy/tracerStudyD3"
-        },{
-          gambar_fasilitas : "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572229/lab_e3c81fd48c.png",
-          nama_fasilitas : "D4 Teknik Informatika",
-          link : "/tracerStudy/tracerStudyD4"
-        }],
+        fasilitas: [
+          {
+            gambar_fasilitas : "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572229/lab_e3c81fd48c.png",
+            nama_fasilitas : "D3 Teknik Informatika",
+            link : "/tracerStudy/tracerStudyD3"
+          },
+          {
+            gambar_fasilitas : "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572229/lab_e3c81fd48c.png",
+            nama_fasilitas : "D4 Teknik Informatika",
+            link : "/tracerStudy/tracerStudyD4"
+          }
+        ],
         carousel : [],
         test_carousel : []
       }
     },
     created() {
-    this.fetchData();
-   },
-   methods: {
-    async fetchData() {
-      console.log("Test")
-      for (let i = 0; i < 5; i++) {
-        console.log(this.fasilitas.gambar_fasilitas[i])
-        this.test_carousel.gambar_fasilitas  = this.fasilitas.gambar_fasilitas[i]
-        this.test_carousel.nama_fasilitas  = this.fasilitas.nama_fasilitas[i];
-        this.carousel.push(this.test_carousel)
-      }
-
-      //console.log(this.carousel)
-      //"",
-      //      "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572314/ruang_uks_1_0101bde366.jpg",
-      //      "https://res.cloudinary.com/kelompok-b7/image/upload/v1653572125/pujas_b2f29f16d7.jpg"
-      //      "",
-      //      "",
-      //      ""
-      //console.log(this.carousel[0].gambar_artikel[0].url)
-      //console.log(this.carousel[1])
+      this.fetchData();
     },
-    
-  },
+    methods: {
+      async fetchData() {
+        for (let i = 0; i < 2; i++) {
+          console.log(this.fasilitas.gambar_fasilitas[i])
+          this.test_carousel.gambar_fasilitas  = this.fasilitas.gambar_fasilitas[i]
+          this.test_carousel.nama_fasilitas  = this.fasilitas.nama_fasilitas[i];
+          this.carousel.push(this.test_carousel)
+        }
+      },
+    },
   }
 </script>
